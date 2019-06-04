ed5303db05e865cbd01eb39d3341072ceef29c06
actanble <actanble@gmail.com>
2019-06-04 12:44:07 +0800

    modify-django-jet

--- add.sh
+++ add.sh
@@ -4,6 +4,11 @@ find . -type d -name __pycache__ | xargs rm -rf
 rm -rf /home/django/xx-scan/tmp/ 
 rm -rf /home/django/xx-scan/datas/ 
 
+
+git config --global user.name "actanble"
+git config --global user.email actanble@gmail.com 
+
+
 git add .
 git commit -m $1 
 git push 
