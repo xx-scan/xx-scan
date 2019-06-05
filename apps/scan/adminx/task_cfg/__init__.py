da042ab4ebf0dfde766862696f5f12ca94d49de9
ibeate <admin@actanble.com>
2019-06-06 07:31:49 +0800

    增加了xadmin的基本设置, 未测试

--- apps/scan/adminx/task_cfg/__init__.py
+++ apps/scan/adminx/task_cfg/__init__.py
@@ -24,13 +24,44 @@ class ScanTaskAdmin(object):
 xadmin.site.register(ScanTask, ScanTaskAdmin)
 
 
+@xadmin.site.register(Workspace)
 class WorkspaceAdmin(object):
+
+    def uniq_name(self, instance):
+        return instance.name + "_" + instance.user.username
+
+    def hosts_count(self, instance):
+        from scan.models import Host
+        return len(Host.objects.filter(workspace=instance))
+
+    uniq_name.short_description = "唯一ID"
+    uniq_name.allow_tags = True
+    uniq_name.is_column = True
+
+    hosts_count.short_description = "主机数统计"
+    hosts_count.allow_tags = True
+    hosts_count.is_column = True
+
     list_editable = ['name', 'summary']
-    list_display = ('name', "user", 'summary', 'date_updated')
+    list_display = ('name', "user", 'summary', 'date_updated', 'hosts_count')
     fieldsets = [("工作组名称", {'fields': ['name']}),
                  ('用户', {'fields': ['user'], 'classes': ['collapse']}), ]
+
+    actions = None
+    # aggregate_fields = {"user_count": "sum", "view_count": "sum"}
+    #
+    # refresh_times = (3, 5, 10)
+    data_charts = {
+        "主机统计": {'title': u"用户空间主机统计", "x-field": "uniq_name", "y-field": ("hosts_count",),
+                       "order": ('date_updated',)},
+    }
+
+
     def save_model(self, request, obj, form, change):
         obj.user = request.user
+
+        # 检查当前访问空间中有没有; 如果有返回403
+
+
         super().save_model(request, obj, form, change)
 
-xadmin.site.register(Workspace, WorkspaceAdmin)
--- apps/scan/xadmin/upgrade.md
+++ apps/scan/xadmin/upgrade.md
@@ -5,3 +5,10 @@ pip install git+git://github.com/sshwsfc/xadmin.git@django2
 pip install xlwt
 
 ```
+# Xadmin
+
+`pip install https://codeload.github.com/sshwsfc/xadmin/zip/django2`
+`pip install xlwt`
+
+# (文档)[https://xadmin.readthedocs.io/en/docs-chinese/]
+- 文档
